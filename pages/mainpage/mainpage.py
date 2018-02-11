26fc8e3f81aa5f2cc5e4028518ef0547d26476d7
Sergii.Skarsh <32761653+Skarshevskyi@users.noreply.github.com>
2018-02-10 20:57:29 -0800

    main changes in the mainpage.py code

--- pages/mainpage/mainpage.py
+++ pages/mainpage/mainpage.py
@@ -10,16 +10,6 @@ class MainPageElements():
         logo = self.driver.find_element(By.CLASS_NAME, 'page-header__logo')
         logo.click()
 
-
-    # def headerButtons(self):
-    #     buttons_list = self.driver.find_elements(By.XPATH, "//ul[@class='sitenav']/li")
-    #     length_of_buttons = len(buttons_list)
-    #     for element in length_of_buttons:
-    #         time.sleep(2)
-    #         self.driverBack()
-    #         buttons_list[element].click()
-    #         buttons_list = self.driver.find_elements(By.XPATH, "//ul[@class='sitenav']/li")
-
     def buttons(self):
         buttons_list = self.driver.find_elements(By.XPATH, "//ul[@class='sitenav']/li")
         for button in range(len(buttons_list)):
@@ -29,21 +19,6 @@ class MainPageElements():
             if len(self.driver.find_elements(By.XPATH, "//ul[@class='sitenav']")) < 1:
                 print("Goind back " + "\n")
                 self.driver.back()
-
-###########_______---------------______---__-_-_-_-_-_-_-_-_-_-
-
-
-    # def headerButtons1(self):
-    #     buttons_list = self.driver.find_elements(By.XPATH, "//li[@class='sitenav-item']/a")
-    #     length_of_buttons = len(buttons_list)
-    #
-    #     for element in range(length_of_buttons):
-    #         buttons_list[element].click()
-    #
-    #         self.driverBack()
-    #         time.sleep(2)
-    #
-    #         buttons_list = self.driver.find_elements(By.XPATH, "//li[@class='sitenav-item']/a")
 
     def whatsAppWeb(self):
         whatsapp_web = self.driver.find_element(By.XPATH, "//li[contains(text(),'Open')]")
